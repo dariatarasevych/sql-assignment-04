965f2c24e9c803a9068bc6d9b14dd5489b5d5269
daratarasevic <tarasevish80@gmail.com>
2026-06-28 14:04:57 +0300

    added additional tasks

--- Script-6.sql
+++ Script-6.sql
@@ -1,3 +1,9 @@
+------------------------------------------
+-- Basic requirements
+-- there is relationships description too
+------------------------------------------
+
+
 -- 1. users: stores user account/user data
 -- Relationships: 1:1 with profiles, 1:Many with orders, reviews, and user_achievements
 CREATE TABLE IF NOT EXISTS users(
@@ -125,6 +131,97 @@ LIMIT 5;
 
 CREATE INDEX IF NOT EXISTS idx_order_items_price ON order_items(price_at_purchace);
 
+
+------------------------------------------
+-- Additional points 
+------------------------------------------
+
+-- 1. Create at least 3 different users for different purposes
+
+CREATE ROLE administrator LOGIN PASSWORD 'Admin123';
+CREATE ROLE analyst LOGIN PASSWORD 'Analyst123';
+CREATE ROLE developer LOGIN PASSWORD 'Developer123';
+
+GRANT CONNECT ON DATABASE gaming_platform TO administrator, analyst, developer; --everyone can connect to this database
+GRANT USAGE ON SCHEMA public TO administrator, analyst, developer; --use schema in this database
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO administrator; -- admin can do everything
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO analyst; -- analyst can only select something to analyse
+GRANT SELECT, INSERT, UPDATE ON games, games_creators, genres, game_genres TO developer; -- developer has access to games, games_creators, genres, game_genres 
+																						 -- to add/update/select their game info
+
+
+-- 2. Create at least 1 view
+-- This view shows catalog that displays essential game details like title, price, release date, and rating
+-- It automatically combines the creator's full name and aggregates all of the game's genres into a single, comma-separated list (string_agg() - підказав AI)
+
+CREATE OR REPLACE VIEW gaming_platform_view AS
+SELECT
+	g.game_id,
+	g.game_title,
+	g.price,
+	g.release_date,
+	g.rating,
+	(gc.first_name || ' ' || gc.last_name) AS developer_name,
+	string_agg(gen.name, ', ') AS game_genres
+FROM games g
+JOIN games_creators gc ON g.creator_id = gc.creator_id
+LEFT JOIN game_genres gg ON g.game_id = gg.game_id
+LEFT JOIN genres gen ON gg.genre_id = gen.genre_id
+GROUP BY g.game_id, g.game_title, g.price, g.release_date, g.rating, gc.first_name, gc.last_name;
+
+
+SELECT * FROM gaming_platform_view LIMIT 100;
+	
+
+-- 3. Create at least 1 stored procedure
+-- THis procedure add money to users wallet, you can see changes in table 'profiles'
+
+CREATE PROCEDURE add_money_to_wallet(
+	p_user_id varchar(36),
+	p_sum_to_add decimal(10, 2)
+)
+AS $$
+BEGIN 
+	if p_sum_to_add <= 0 then
+		raise exception 'Sum cannot be less then 0';
+	end if;
+	
+	update profiles
+	set wallet = wallet + p_sum_to_add
+	where user_id = p_user_id;
+END;
+$$ LANGUAGE plpgsql;
+
+CALL add_money_to_wallet('a6a4a148-b938-440f-8cd6-f01bd55de392', 50); --$11.49
+
+SELECT * FROM profiles WHERE user_id = 'a6a4a148-b938-440f-8cd6-f01bd55de392'; --$61.49
+
+
+-- 4. Create at least 1 trigger or function 
+--This trigger executes immediately after a row is added to order_items (user made a purchase), 
+--automatically inserting a new record into user_achievements with 0 hours and 0 achievements for this game
+
+CREATE OR REPLACE FUNCTION trigger_update_achievments()
+RETURNS TRIGGER AS $$
+DECLARE 
+	v_user_id varchar(36);
+BEGIN
+	--Через order_id дізнається user_id, бо в order_items немає user_id
+	select user_id into v_user_id
+	from orders 
+	where order_id = new.order_id;
+
+	insert into user_achievements(user_id, game_id, playtime_hours, achievements_unlocked)
+	values(v_user_id, game_id, 0, 0);
+	return null;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER after_game_purchase
+AFTER INSERT ON order_items
+FOR EACH ROW
+EXECUTE FUNCTION trigger_update_achievments();
 
 
 
